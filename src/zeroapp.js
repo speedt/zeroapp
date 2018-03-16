5c1284dde263d7d3533f5f1ed286a553605d6875
speedt <13837186852@qq.com>
2018-03-16 17:11:22 +0800

    haha

--- src/zeroapp.js
+++ src/zeroapp.js
@@ -7,3 +7,23 @@
 
 var fs = require('fs'),
     path = require('path');
+
+var application = require('./application');
+
+var ZeroApp = module.exports = {
+  version: require('../package.json').version,
+};
+
+var self = this;
+
+var load = function(path, name){
+  if(name) return require(path + name);
+  return require(path);
+};
+
+ZeroApp.createApp = function(opts, cb){
+  var app = application;
+  app.init(opts);
+  self.app = app;
+  cb.bind(app)();
+};
