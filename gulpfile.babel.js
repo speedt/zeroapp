73f527df22becf55f9046c91e7813546fa25ab88
speedt <13837186852@qq.com>
2018-03-16 16:40:04 +0800

    haha

---
+++ gulpfile.babel.js
@@ -0,0 +1,15 @@
+/*!
+ * zeroapp
+ * Copyright(c) 2016 huangxin <3203317@qq.com>
+ * MIT Licensed
+ */
+'use strict';
+
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var clean = require('gulp-clean');
+var uglify = require('gulp-uglify');
+
+gulp.task('clean', () => {
+  return gulp.src('dist/*').pipe(clean({ force: true }));
+});
